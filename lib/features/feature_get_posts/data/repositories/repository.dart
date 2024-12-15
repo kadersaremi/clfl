ff8c509d896bfa3ee8d6ac75b66641d58d9b596d
kadersaremi <kadersaremi@gmail.com>
2024-12-15 16:11:00 +0330

    feat: add PostsRepositoryImplementation class

---
+++ lib/features/feature_get_posts/data/repositories/repository.dart
@@ -0,0 +1,9 @@
+import 'package:clfl/features/feature_get_posts/domain/repositories/repositories.dart';
+
+class PostsRepositoryImplementation implements PostsRepository {
+  @override
+  Future<List<PostsRepository>> getPosts() {
+    // TODO: implement getPosts
+    throw UnimplementedError();
+  }
+}
