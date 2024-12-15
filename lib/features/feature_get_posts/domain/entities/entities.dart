3a95ed25ccb41aef8c9aeb6b301ca35f2f453cff
kadersaremi <kadersaremi@gmail.com>
2024-12-15 14:19:49 +0330

    feat: add PostEntities class based on JSONPlaceholder posts

---
+++ lib/features/feature_get_posts/domain/entities/entities.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class PostEntities extends Equatable {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+  const PostEntities({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  @override
+  List<Object?> get props => [
+        userId,
+        id,
+        title,
+        body,
+      ];
+}
